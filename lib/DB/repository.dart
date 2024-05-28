b8fe5cb8638d3af5d4549cb29b191bcfcca5cd03
0soi <shcho1225@naver.com>
2024-05-28 17:37:55 +0900

    add ZandiInfo update & insert

--- lib/DB/repository.dart
+++ lib/DB/repository.dart
@@ -17,17 +17,14 @@ class TimeBoxingRepository extends ChangeNotifier {
     return _timeBoxingRepository;
   }
 
-  // insert
+
+  // TimeBoxingInfo insert
   Future<int> insertTimeBoxing(DateTime date, String task, int priority, int startTime, int endTime) {
     return _myDatabase.into(_myDatabase.timeBoxingInfo).insert(
         TimeBoxingInfoCompanion.insert(date: date, task: task, priority: priority, startTime: startTime, endTime: endTime));
   }
-
-  Future<int> insertZandiInfo(DateTime date, int stack) {
-    return _myDatabase.into(_myDatabase.zandiInfo).insert(ZandiInfoCompanion.insert(date: date, stack: stack));
-  }
   
-  //읽기
+  // select TimeBoxingInfo
   Future<List<TimeBoxingInfoData>> selectTimeBoxing(DateTime date) {
     final result = (_myDatabase.select(_myDatabase.timeBoxingInfo)..where((t) =>
     t.date.year.equals(date.year) &
@@ -37,6 +34,24 @@ class TimeBoxingRepository extends ChangeNotifier {
     return result;
   }
 
+  // insert ZandiInfo FirstTime
+  Future<int> insertZandiInfo_FirstTime(DateTime date) {
+    return _myDatabase.into(_myDatabase.zandiInfo).insert(ZandiInfoCompanion.insert(date: date, stack: 0));
+  }
+
+  // insert ZandiInfo increaseStack
+  Future<int> insertZandiInfo_increaseStack(DateTime date, int stack) {
+    return _myDatabase.into(_myDatabase.zandiInfo).insert(ZandiInfoCompanion.insert(date: date, stack: stack));
+  }
+
+  // update ZandiInfo
+  Future<int> updateZandiInfo(DateTime date, int stack) {
+    return (
+      _myDatabase.update(_myDatabase.zandiInfo)..where((t) => t.date.equals(date))).write(ZandiInfoCompanion(stack: Value(stack))
+      );
+  }
+
+  // select ZandiInfo
   Future<List<ZandiInfoData>> selectZandi(DateTime date) {
     final result = (_myDatabase.select(_myDatabase.zandiInfo)..where((t) =>
     t.date.year.equals(date.year) &
