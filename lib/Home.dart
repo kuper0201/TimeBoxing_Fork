4ec05c8901f9f71ebc746c93bf23a18df9b3511b
kuper0201 <kuper171@gmail.com>
2024-06-02 17:30:26 +0900

    future builder

--- lib/Home.dart
+++ lib/Home.dart
@@ -61,74 +61,76 @@ class _HomeViewState extends State<HomeView> {
         if(!snapshot.hasData) {
           return const CircularProgressIndicator();
         } else {
-          return Column(
-            children: [
-              Card(child: buildPlanTile(current!.task, current!.startTime, current!.endTime)),
-              const SizedBox(
-                width: double.infinity,
-                child: Padding(padding: EdgeInsets.only(left: 15, top: 20), child: Text("다음 일정", style: TextStyle(fontSize: 25), textAlign: TextAlign.left)),
-              ),
-              Card(child: buildPlanTile("일정 이름", 100, 200)),
-              Card(child: buildPlanTile("일정 이름", 100, 200)),
-              Padding(
-                padding: const EdgeInsets.only(top: 5),
-                child: Card(
-                  child: SizedBox(
-                    height: 60,
-                    width: double.infinity,
-                    child: InkWell(
-                      onTap: () {
-                        // DB Query 후 PlanView에 넘겨 줄 것
-                        // Navigator.push(context, MaterialPageRoute(builder: (context) => const PlanView()));
-                      },
-                      child: const Center(
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Icon(Icons.edit),
-                            Text(
-                              "수정",
-                              style: TextStyle(fontSize: 18),
-                              textAlign: TextAlign.center,
-                            ),
-                          ],
+          if(snapshot.data!.isNotEmpty) {
+            current = snapshot.data![0];
+            return Column(
+              children: [
+                Card(child: buildPlanTile(current!.task, current!.startTime, current!.endTime)),
+                const SizedBox(
+                  width: double.infinity,
+                  child: Padding(padding: EdgeInsets.only(left: 15, top: 20), child: Text("다음 일정", style: TextStyle(fontSize: 25), textAlign: TextAlign.left)),
+                ),
+                Card(child: buildPlanTile("일정 이름", 100, 200)),
+                Card(child: buildPlanTile("일정 이름", 100, 200)),
+                Padding(
+                  padding: const EdgeInsets.only(top: 5),
+                  child: Card(
+                    child: SizedBox(
+                      height: 60,
+                      width: double.infinity,
+                      child: InkWell(
+                        onTap: () {
+                          // DB Query 후 PlanView에 넘겨 줄 것
+                          // Navigator.push(context, MaterialPageRoute(builder: (context) => const PlanView()));
+                        },
+                        child: const Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Icon(Icons.edit),
+                              Text(
+                                "수정",
+                                style: TextStyle(fontSize: 18),
+                                textAlign: TextAlign.center,
+                              ),
+                            ],
+                          ),
                         ),
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
-          );
+              ],
+            );
+          } else {
+            return InkWell(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) => const StepViewPage()));
+              },
+              child: Ink(
+                width: 300,
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Colors.lightGreen,
+                  borderRadius: BorderRadius.circular(15)
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(40),
+                  child: Center(
+                    child: Column(
+                      children: [
+                        Expanded(child: Text("일정 추가", style: TextStyle(fontSize: 32))),
+                        Expanded(child: Icon(Icons.add, size: 60,))
+                      ]
+                    ),
+                  ),
+                )
+              )
+            );
+          }
         }
       },
     );
-
-    // 일정 없음
-    // return InkWell(
-    //   onTap: () {
-    //     Navigator.push(context, MaterialPageRoute(builder: (context) => const StepViewPage()));
-    //   },
-    //   child: Ink(
-    //     width: 300,
-    //     height: 200,
-    //     decoration: BoxDecoration(
-    //       color: Colors.lightGreen,
-    //       borderRadius: BorderRadius.circular(15)
-    //     ),
-    //     child: const Padding(
-    //       padding: EdgeInsets.all(40),
-    //       child: Center(
-    //         child: Column(
-    //           children: [
-    //             Expanded(child: Text("일정 추가", style: TextStyle(fontSize: 32))),
-    //             Expanded(child: Icon(Icons.add, size: 60,))
-    //           ]
-    //         ),
-    //       ),
-    //     )
-    //   )
-    // );
   }
 
   @override
