cd856ed1d6bcaeba1d3255df53c4c4a709ea5ef1
SP3cTRum-GH <min1324354@gmail.com>
2024-06-07 14:49:18 +0900

    edit db

--- lib/DB/database.dart
+++ lib/DB/database.dart
@@ -5,15 +5,21 @@ import 'package:drift/native.dart';
 import 'package:time_boxing/DB/models.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
+import 'package:time_boxing/DB/repositoryForTimeBoxing.dart';
+import 'package:time_boxing/DB/repositoryForZandi.dart';
 
 part 'database.g.dart';
 
 @DriftDatabase(tables: [TimeBoxingInfo,ZandiInfo])
 class Mydatabase extends _$Mydatabase {
-  Mydatabase() : super(_openDb());
+   Mydatabase._internal() : super(_openDb());
 
+  static final Mydatabase instance = Mydatabase._internal();
   @override
   int get schemaVersion => 1;
+
+  TimeBoxingRepository get timeBoxingRepository => TimeBoxingRepository(this);
+  RepositoryForZandi get zandiRepository => RepositoryForZandi(this);
 }
 
 LazyDatabase _openDb() {
