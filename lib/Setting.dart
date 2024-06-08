e53e233f9351696f6c8ddda96e7713fbb305976a
0soi <shcho1225@naver.com>
2024-06-08 15:22:29 +0900

    test

--- lib/Setting.dart
+++ lib/Setting.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:time_boxing/DB/database.dart';
+import 'package:drift_db_viewer/drift_db_viewer.dart';
 
 class SettingView extends StatefulWidget {
   const SettingView({super.key});
@@ -8,9 +9,13 @@ class SettingView extends StatefulWidget {
   State<SettingView> createState() => _SettingViewState();
 }
 
+Mydatabase db = Mydatabase.instance;
+
 class _SettingViewState extends State<SettingView> {
   @override
   Widget build(BuildContext context) {
+    
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) => DriftDbViewer(db)));
     
     return Scaffold(  
       body: Center(
