5637005d39f01a74e94f58d58b61250ca55b62d3
kuper0201 <kuper171@gmail.com>
2024-06-11 17:18:50 +0900

    remove Navigator.push on Setting view

--- lib/Setting.dart
+++ lib/Setting.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:drift_db_viewer/drift_db_viewer.dart';
 import 'package:time_boxing/DB/database.dart';
 
 Mydatabase db = Mydatabase.instance;
@@ -16,7 +15,6 @@ class SettingView extends StatefulWidget {
 class _SettingViewState extends State<SettingView> {
   @override
   Widget build(BuildContext context) {
-    Navigator.of(context).push(MaterialPageRoute(builder: (context) => DriftDbViewer(db)));
     return Scaffold(  
       body: Center(
         child: Container(child: Text("Setting"),),
