347f891bb57b773366b42cc78569ed85398e1332
kuper0201 <kuper171@gmail.com>
2024-06-13 18:09:19 +0900

    update history

--- README.md
+++ README.md
@@ -1,16 +1,39 @@
-# time_boxing
+# Time Boxing
 
-A new Flutter project.
+<img src="https://img.shields.io/badge/Dart-0175C2?style=for-the-badge&logo=Dart&logoColor=white"> <img src="https://img.shields.io/badge/Flutter-02569B?style=for-the-badge&logo=Flutter&logoColor=white"> <img src="https://img.shields.io/badge/Android-34A853?style=for-the-badge&logo=Android&logoColor=white"> <img src="https://img.shields.io/badge/Windows-0078D4?style=for-the-badge&logo=Windows&logoColor=white"> <img src="https://img.shields.io/badge/Linux-FCC624?style=for-the-badge&logo=Linux&logoColor=white"> <img src="https://img.shields.io/badge/MacOS-FA243C?style=for-the-badge&logo=Apple&logoColor=white">
 
-## Getting Started
+> A cross platform tool for Time Boxing.
 
-This project is a starting point for a Flutter application.
+## Preview
 
-A few resources to get you started if this is your first Flutter project:
+<div align="center">
+  <table>
+    <tr align="center">
+      <th>Encryption</th>
+      <th>Decryption</th>
+      <th>Input Password</th>
+    </tr>
+    <tr align="center">
+      <td><img src="ScreenShot/encrypt.jpg" width="250"/></td>
+      <td><img src="ScreenShot/decrypt.jpg" width="250"/></td>
+      <td><img src="ScreenShot/password.jpg" width="250"/></td>
+    </tr>
+  </table>
+</div>
 
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+## Features
 
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+- Time Boxing
+- Supporting cross-platform(tested with Linux, macOS, Android, Windows10)
+
+## Install
+
+- Windows, macOS, Linux, Android
+
+  - <a href="https://github.com/kuper0201/TimeBoxing_Flutter/releases/latest">Github Release</a>
+
+## Change Log
+
+2024.06.11.
+
+  - First Release
--- lib/History.dart
+++ lib/History.dart
@@ -1,6 +1,7 @@
 import 'dart:ffi';
 
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 import 'package:time_boxing/DB/database.dart';
 import 'package:time_boxing/MoreHistoryView.dart';
 
@@ -25,28 +26,31 @@ class _HistoryViewState extends State<HistoryView> {
 
   //최대 stack 저장빈값
   int maxstack = 0;
+  
+  int check = 0;
+  int uncheck = 0;
 
   Mydatabase db = Mydatabase.instance;
 
-//get today
-DateTime today = DateTime(
-  DateTime.now().year,
-  DateTime.now().month,
-  DateTime.now().day,
-);
+  //get today
+  DateTime today = DateTime(
+    DateTime.now().year,
+    DateTime.now().month,
+    DateTime.now().day,
+  );
 
-//Zandi MaxStack
-Future<int> getMaxStack() async {
-  final result = await db.zandiRepository.selectZandi35MaxStack();
-  final maxstack = result.first.stack;
-  return maxstack;
-}
+  //Zandi MaxStack
+  Future<int> getMaxStack() async {
+    final result = await db.zandiRepository.selectZandi35MaxStack();
+    final maxstack = result.first.stack;
+    return maxstack;
+  }
 
-//get ZandiList 35 ago
-Future<List<ZandiInfoData>> getZandi35Ago() async {
-  final result = await db.zandiRepository.selectZandi35DaysAgo(today.subtract(const Duration(days: 34)));
-  return result;
-}
+  //get ZandiList 35 ago
+  Future<List<ZandiInfoData>> getZandi35Ago() async {
+    final result = await db.zandiRepository.selectZandi35DaysAgo(today.subtract(const Duration(days: 34)));
+    return result;
+  }
 
   Widget getCustomTable() {
     double cellSize = MediaQuery.of(context).size.width/10; // 칸의 크기를 화면 너비의 1/10로 설정
@@ -57,13 +61,14 @@ Future<List<ZandiInfoData>> getZandi35Ago() async {
       builder: (context, snapshot) {
         if (!snapshot.hasData) {
           //로딩 애니메이션
-          return CircularProgressIndicator();
+          return const CircularProgressIndicator();
         } else if (snapshot.hasError) {
-          return Padding(padding: const EdgeInsets.all(8.0),
-          child: Text(
-            //에러일경우 에러메세지 출력
-            'Error ${snapshot.error}',
-            style: TextStyle(fontSize: 15)
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              //에러일경우 에러메세지 출력
+              'Error ${snapshot.error}',
+              style: const TextStyle(fontSize: 15)
             ),
           );
         } else {
@@ -71,71 +76,99 @@ Future<List<ZandiInfoData>> getZandi35Ago() async {
           List<ZandiInfoData> ZandiData = snapshot.data![0] as List<ZandiInfoData>;
           maxstack = snapshot.data![1] as int;
 
-
           //연속일자 값 저장 최근값이 오늘날짜인지 확인 list.first.date에 현재스택값-1 값 더한값이 오늘이랑 같을경우 연속일자 가져옴
           if((ZandiData[ZandiData.length-1].date.add(Duration(days: ZandiData[ZandiData.length-1].stack - 1)))==today) {
-              currentStack = ZandiData[ZandiData.length-1].stack;
+            currentStack = ZandiData[ZandiData.length-1].stack;
           }
 
           int maxStreakSize = 35;
           List<ZandiInfoConvert> converted = ZandiData.map((d) => ZandiInfoConvert(date: (today.difference(d.date).inDays), stack: d.stack)).toList();
           List<bool> items = List.generate(maxStreakSize, (idx) => false);
             
+          check = 0;
+          uncheck = 0;
+
           int idx = 0;
           label : for(final d in converted) {
             idx = d.date - d.stack + 1;
             for(int i = 0; i < d.stack; i++) {
               if(idx >= maxStreakSize) break label;
-                items[idx] = true;
-                idx++;
+              check += 1;
+              items[idx] = true;
+              idx++;
             }
           }
 
+          uncheck = maxStreakSize - check;
+
           //테이블 그리기
           return Column(
+            children:[
+              Expanded(flex: 1, child: Container(width: double.infinity, child: Text("연속$currentStack일 진행중입니다", textAlign: TextAlign.start))),
+              Expanded(
+                flex: 9,
+                child: Table(
+                  border: TableBorder.all(),
+                  children: List<TableRow>.generate(6, (rowIndex) {
+                    if(rowIndex == 0) {
+                      return TableRow(
+                        children: List.generate(7, (colIndex) {
+                          if(colIndex == 0) return Container(child: Center(child: Text(style: TextStyle(fontSize: MediaQuery.of(context).size.width*0.3/15), "Today", textAlign: TextAlign.center,)));
+                          return Container();
+                        }),
+                      );
+                    }
 
-           children:[
-            Container(width: double.infinity, child: Text("연속$currentStack일 진행중입니다")),
-            Container(width: double.infinity, padding: EdgeInsets.only(left: 5+MediaQuery.of(context).size.width/20,top:10), child: Text(style: TextStyle(fontSize: MediaQuery.of(context).size.width*0.3/15),"Today")),
-            Table(
-            border: TableBorder.all(),
-            children: List<TableRow>.generate(5, (rowIndex) {
-              return TableRow(
-                children: List<Widget>.generate(7, (colIndex) {
-                  int itemIndex = rowIndex * 7 + colIndex;
-                  // 리스트의 빈값에 false초기화
-                  bool item = itemIndex < items.length ? items[itemIndex] : false;
-                  bool isFirstItem = itemIndex == 0;
-                  return Container(
-                    width: cellSize,
-                    height: screenHeight,
-                    alignment: Alignment.center,
-                    color: isFirstItem ? Colors.yellow : Colors.white,
-                    child: Icon(
-                      item ? Icons.check : Icons.close,
-                      color: item ? Colors.green : Colors.red,
-                    ),
-                  );
-                }),
-               );
-            }),
-          ),
-           Container(padding: EdgeInsets.only(top: 10), width: double.infinity, child: Text("최대스택: $maxstack")),
-          ]);
+                    return TableRow(
+                      children: List<Widget>.generate(7, (colIndex) {
+                        int itemIndex = rowIndex * 7 + colIndex;
+                        // 리스트의 빈값에 false초기화
+                        bool item = itemIndex < items.length ? items[itemIndex] : false;
+                        bool isFirstItem = itemIndex == 0;
+                        return Container(
+                          width: cellSize,
+                          height: screenHeight,
+                          alignment: Alignment.center,
+                          color: isFirstItem ? Colors.yellow : Colors.white,
+                          child: Icon(
+                            item ? Icons.check : Icons.close,
+                            color: item ? Colors.green : Colors.red,
+                          ),
+                        );
+                      }),
+                    );
+                  }),
+                ),
+              ),
+              Container(padding: const EdgeInsets.only(top: 10), width: double.infinity, child: Text("최대스택: $maxstack")),
+              Expanded(flex: 2, child: TextButton(onPressed:(){ Navigator.push(context, MaterialPageRoute(builder: (context) => const MoreHistoryView(),));}, child: const Text("더보기"))),
+              Expanded(
+                flex: 11,
+                child: PieChart(
+                  initialAngleInDegree: -90,
+                  colorList: const [Colors.green, Colors.red],
+                  dataMap: {'했음': check.toDouble(), '안했음': uncheck.toDouble()},
+                  legendOptions: const LegendOptions(
+                    legendPosition: LegendPosition.bottom,
+                    showLegendsInRow: true
+                  ),
+                  chartValuesOptions: const ChartValuesOptions(
+                    showChartValueBackground: false,
+                    decimalPlaces: 0
+                  ),
+                )
+              ),
+            ]
+          );
         }
       }
     );
- }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(  
-      body: Column(
-          children: [
-            Container(padding: EdgeInsets.only(left:10,right: 10), child: getCustomTable()),
-            TextButton(onPressed:(){  Navigator.push(context, MaterialPageRoute(builder: (context) => const MoreHistoryView(),));}, child: Text("더보기")),
-            Container(padding: EdgeInsets.only(top:50), child: Image.asset('assets/images/crown.png',height: 128,width: 128))
-          ],
-      )
+    return Scaffold(
+      body: Container(padding: const EdgeInsets.only(left:10,right: 10), child: getCustomTable()),
     );
   }
 }
